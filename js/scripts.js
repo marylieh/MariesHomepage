974505f91297401516f9c65ec5eae1c674c4e856
asmax15 <max.aschmann@gmail.com>
2021-08-18 14:06:32 +0200

    Replace lorem ipsum with real text

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,17 @@ $(document).ready(function(){
 
     $(".goTop").click(function(){scroll(0,0)});
 
+    $("#mc.service").click(function (){window.location = "https://dev.bukkit.org/members/***REMOVED***25/projects"});
+    $("#git.service").click(function (){window.location = "https://github.com/***REMOVED***15"});
+    $("#db.service").click(function (){window.location = "https://www.mongodb.com/"});
+    $("#wd.service").click(function (){window.location = "https://github.com/***REMOVED***15/***REMOVED***Homepage"});
+    $("#design.service").click(function (){window.location = "https://github.com/***REMOVED***15/***REMOVED***Homepage/blob/main/css/style.css"});
+    $("#android.service").click(function (){window.location = "https://play.google.com/store"});
+
+    $("#discord.item").click(function (){window.location = "https://discord.gg/YeSM2c6qwb"});
+    $("#mail.item").click(function (){window.location = "mailto:contact@***REMOVED***.me"});
+    $("#bukkit.item").click(function (){window.location = "https://dev.bukkit.org/members/***REMOVED***25/reputation"});
+
     $('.menu-toggler').click(function(){
         $(this).toggleClass("active");
         $(".navbar-menu").toggleClass("active");
