a855f154ec344ed2a6ec878cf6ce1bdd42f9a335
marylieh <max.aschmann@gmail.com>
2022-11-25 21:53:28 +0100

    Change Something

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
 
     $(".goTop").click(function(){scroll(0,0)});
 
-    $("#mc.service").click(function (){window.location = "https://dev.bukkit.org/members/***REMOVED***25/projects"});
-    $("#git.service").click(function (){window.location = "https://github.com/***REMOVED***15"});
+    $("#mc.service").click(function (){window.location = "https://dev.bukkit.org/members/marylieh/projects"});
+    $("#git.service").click(function (){window.location = "https://github.com/marylieh"});
     $("#db.service").click(function (){window.location = "https://www.mongodb.com/"});
-    $("#wd.service").click(function (){window.location = "https://github.com/***REMOVED***15/***REMOVED***Homepage"});
-    $("#design.service").click(function (){window.location = "https://github.com/***REMOVED***15/***REMOVED***Homepage/blob/main/css/style.css"});
+    $("#wd.service").click(function (){window.location = "https://github.com/marylieh/MariesHomepage"});
+    $("#design.service").click(function (){window.location = "https://github.com/marylieh/MariesHomepage/blob/main/css/style.css"});
     $("#android.service").click(function (){window.location = "https://play.google.com/store"});
 
     $("#discord.item").click(function (){window.location = "https://discord.gg/YeSM2c6qwb"});
-    $("#mail.item").click(function (){window.location = "mailto:contact@***REMOVED***.me"});
-    $("#bukkit.item").click(function (){window.location = "https://dev.bukkit.org/members/***REMOVED***25/reputation"});
+    $("#mail.item").click(function (){window.location = "mailto:contact@marylieh.me"});
+    $("#bukkit.item").click(function (){window.location = "https://dev.bukkit.org/members/marylieh/reputation"});
 
     $('.menu-toggler').click(function(){
         $(this).toggleClass("active");
